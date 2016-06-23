0db51f9801b61d834de1884e0dd510c7ab035e08
juliasilge <julia.silge@gmail.com>
2016-06-23 13:41:50 -0600

    Change arguments/output of weight_age

--- tests/testthat/test-weight-age.R
+++ tests/testthat/test-weight-age.R
@@ -5,7 +5,6 @@ texas <- geo.make(state = "TX")
 data(texassurvey)
 
 test_that("error handling for weighting indicator is working", {
-        expect_error(weight_age(texassurvey, texas, 
-                                response, howabouthemapples),
+        expect_error(weight_age(texassurvey, texas, howabouthemapples),
                      "indicator must be one of sex, raceethnicity, or age")
 })
