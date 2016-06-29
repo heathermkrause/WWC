93c008fc3c9317a4b7bff5e7554373bf840c7052
juliasilge <julia.silge@gmail.com>
2016-06-29 14:33:07 -0600

    Fix up globals

--- R/globals.R
+++ R/globals.R
@@ -1,3 +1,3 @@
 globalVariables(c("population", "raceethnicity", "sex", "notother", "education",
-                  "age", "sextotal", "geototal", "value", "yes", "no", "prop",
+                  "age", "sextotal", "geototal", "value", "yes", "no", "prob",
                   "age_wt", "race_wt", "sex_wt", "edu_wt", "wt"))
