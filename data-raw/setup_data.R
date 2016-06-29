afd0ac1bacd9e3a832805aa5773e1ec72cb8f12f
juliasilge <julia.silge@gmail.com>
2016-06-29 14:36:06 -0600

    Change to 'prop' to specify survey characteristics, in setup_data and new version of texassurvey

--- data-raw/setup_data.R
+++ data-raw/setup_data.R
@@ -12,19 +12,18 @@ devtools::use_data(countyFIPS, overwrite = TRUE)
 # texassurvey data set ----------------------------------------------
 
 library(WWC)
-texas <- geo.make(state = "TX")
-prob_sex <- c(0.55, 0.45)
-prob_raceethnicity <- c(0.75, 0.15, 0.03, 0.02, 0.05)
-prob_age <- c(0, 0, 0, 0, 0.05, 0.09, 0.09, 0.1, 0.18, 0.16, 0.14, 0.09, 0.06, 0.04)
-prob_education <- c(0.18, 0.25, 0.29, 0.28)
+prop_sex <- c(0.55, 0.45)
+prop_raceethnicity <- c(0.75, 0.15, 0.03, 0.02, 0.05)
+prop_age <- c(0, 0, 0, 0, 0.05, 0.09, 0.09, 0.1, 0.18, 0.16, 0.14, 0.09, 0.06, 0.04)
+prop_education <- c(0.18, 0.25, 0.29, 0.28)
 weight_sex <- c(0.5, 2)
 weight_raceethnicity <- c(0.2, 2, 1, 1, 1)
 weight_age <- c(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1)
 weight_education <- c(1, 1, 1, 1)
-texassurvey <- simulate_survey(prob_sex, weight_sex,
-                               prob_raceethnicity, weight_raceethnicity,
-                               prob_age, weight_age,
-                               prob_education, weight_education,
+texassurvey <- simulate_survey(prop_sex, weight_sex,
+                               prop_raceethnicity, weight_raceethnicity,
+                               prop_age, weight_age,
+                               prop_education, weight_education,
                                n = 1000)
 devtools::use_data(texassurvey, overwrite = TRUE)
 
