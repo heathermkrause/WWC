009046d9ddbe8970b4a474376e12493463482ec9
juliasilge <julia.silge@gmail.com>
2016-07-06 15:36:28 -0600

    More globals needed

--- R/globals.R
+++ R/globals.R
@@ -1,3 +1,5 @@
 globalVariables(c("population", "raceethnicity", "sex", "notother", "education",
                   "age", "sextotal", "geototal", "value", "yes", "no", "prob",
-                  "age_od", "race_od", "sex_od", "edu_od", "odds"))
+                  "age_od", "race_od", "sex_od", "edu_od", "odds",
+                  "age_lambda", "race_lambda", "sex_lambda", "education_lambda",
+                  "response"))
