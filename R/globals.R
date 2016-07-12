9427d37ed8dfc964f62ac649b9bb1dc2c52aae3d
juliasilge <julia.silge@gmail.com>
2016-07-12 11:37:07 -0600

    Globals! Utils! Oh my!

--- R/globals.R
+++ R/globals.R
@@ -1,5 +1,5 @@
 globalVariables(c("population", "raceethnicity", "sex", "notother", "education",
                   "age", "sextotal", "geototal", "value", "yes", "no", "prob",
-                  "age_od", "race_od", "sex_od", "edu_od", "odds",
+                  "age_od", "race_od", "sex_od", "edu_od", "odds", "region",
                   "age_lambda", "race_lambda", "sex_lambda", "education_lambda",
-                  "response"))
+                  "response", "acsedutable", "acsagetable"))
--- R/utils.R
+++ R/utils.R
@@ -1,12 +1,9 @@
-col_name <- function (x, default = stop("Please supply column name", call. = FALSE))
+# utility function from tidyr::col_name
+col_name <- function (x, default = stop("Please supply row/column name", call. = FALSE))
 {
-        if (is.character(x))
-                return(x)
-        if (identical(x, quote(expr = )))
-                return(default)
-        if (is.name(x))
-                return(as.character(x))
-        if (is.null(x))
-                return(x)
-        stop("Invalid column specification", call. = FALSE)
+        if (is.character(x)) return(x)
+        if (identical(x, quote(expr = ))) return(default)
+        if (is.name(x)) return(as.character(x))
+        if (is.null(x)) return(x)
+        stop("Invalid row/column specification", call. = FALSE)
 }
