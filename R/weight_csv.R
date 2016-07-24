fbb0f1122ee84af08f7241f268d9c7d42d535754
juliasilge <julia.silge@gmail.com>
2016-07-23 20:33:20 -0600

    Update weight_csv for csv output (as well as input)

--- R/weight_csv.R
+++ R/weight_csv.R
@@ -1,27 +1,29 @@
-#' A wrapper for weight_wwc for CSV file input
+#' A wrapper for weight_wwc for CSV file input/output
 #' 
-#' @param file A file containing the survey data. Either a path to a file, a 
-#' connection, or literal data (either a single string or a raw vector). Files 
+#' @param file A file containing the input survey data. Either a path to a file, 
+#' a connection, or literal data (either a single string or a raw vector). Files 
 #' starting with \code{http://}, \code{https://}, \code{ftp://}, or 
 #' \code{ftps://} will be automatically downloaded, and zipped files wll be 
 #' uncompressed. 
+#' @param path Path to write a csv file of the output weighted results. The new 
+#' file will contain the original survey data with 1 column added, 
+#' \code{weight}, the post-stratification weight for each row in the survey.
 #' @param ... Arguments to be passed to \code{weight_wwc}, geographical region 
 #' and weighting indicators. See \code{weight_wwc} for more details.
-#' 
-#' @return A data frame, the original survey data with 1 column added, 
-#' \code{weight}, the post-stratification weight for each row in the survey.
 #' 
 #' @details See \code{weight_wwc} for more details on the weighting algorithm
 #' arguments
 #' 
 #' @name weight_csv
 #' 
-#' @examples 
-#' weight_csv(system.file("extdata/examplesurvey.csv", package = "WWC"), TX, sex, raceethnicity)
+#' @examples
+#' tmp <- tempfile() 
+#' weight_csv(system.file("extdata/examplesurvey.csv", package = "WWC"), tmp,
+#'      TX, sex, raceethnicity)
 #' 
 #' @export
 
-weight_csv <- function(file, ...) {
+weight_csv <- function(file, path, ...) {
         mysurvey <- readr::read_csv(file)
-        weight_wwc(mysurvey, ...)
+        readr::write_csv(weight_wwc(mysurvey, ...), path)
 }
