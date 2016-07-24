f59f20904ea4f0976b6ed7f7e0c61297f745ea98
juliasilge <julia.silge@gmail.com>
2016-07-23 20:34:53 -0600

    Clarify input/output in weight_csv

--- R/weight_csv.R
+++ R/weight_csv.R
@@ -1,12 +1,12 @@
 #' A wrapper for weight_wwc for CSV file input/output
 #' 
-#' @param file A file containing the input survey data. Either a path to a file, 
-#' a connection, or literal data (either a single string or a raw vector). Files 
-#' starting with \code{http://}, \code{https://}, \code{ftp://}, or 
+#' @param inputfile A file containing the input survey data. Either a path to a 
+#' file, a connection, or literal data (either a single string or a raw vector). 
+#' Files starting with \code{http://}, \code{https://}, \code{ftp://}, or 
 #' \code{ftps://} will be automatically downloaded, and zipped files wll be 
 #' uncompressed. 
-#' @param path Path to write a csv file of the output weighted results. The new 
-#' file will contain the original survey data with 1 column added, 
+#' @param outputpath Path to write a csv file of the output weighted results. 
+#' The new file will contain the original survey data with 1 column added, 
 #' \code{weight}, the post-stratification weight for each row in the survey.
 #' @param ... Arguments to be passed to \code{weight_wwc}, geographical region 
 #' and weighting indicators. See \code{weight_wwc} for more details.
@@ -23,7 +23,7 @@
 #' 
 #' @export
 
-weight_csv <- function(file, path, ...) {
-        mysurvey <- readr::read_csv(file)
-        readr::write_csv(weight_wwc(mysurvey, ...), path)
+weight_csv <- function(inputfile, outputpath, ...) {
+        mysurvey <- readr::read_csv(inputfile)
+        readr::write_csv(weight_wwc(mysurvey, ...), outputpath)
 }
