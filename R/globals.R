e34719d58681dd5ae6b7affba51ece19cc421d86
juliasilge <julia.silge@gmail.com>
2016-07-25 15:24:39 -0600

    GLOBALS!!!

--- R/globals.R
+++ R/globals.R
@@ -2,4 +2,5 @@ globalVariables(c("population", "raceethnicity", "sex", "notother", "education",
                   "age", "sextotal", "geototal", "value", "yes", "no", "prob",
                   "age_od", "race_od", "sex_od", "edu_od", "odds", "region",
                   "age_lambda", "race_lambda", "sex_lambda", "education_lambda",
-                  "response", "acsedutable", "acsagetable"))
+                  "response", "acsedutable", "acsagetable",
+                  "state", "county", "FIPS"))
