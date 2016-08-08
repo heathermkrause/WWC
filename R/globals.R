2410a05dddf0a9a1090f4c09a1cb09df55e995e3
juliasilge <julia.silge@gmail.com>
2016-08-08 11:35:53 -0600

    Need some geo things in globals

--- R/globals.R
+++ R/globals.R
@@ -3,4 +3,5 @@ globalVariables(c("population", "raceethnicity", "sex", "notother", "education",
                   "age_od", "race_od", "sex_od", "edu_od", "odds", "region",
                   "age_lambda", "race_lambda", "sex_lambda", "education_lambda",
                   "response", "acsedutable", "acsagetable",
+                  "geo_lambda", "geo_od", "geoprob",
                   "state", "county", "FIPS"))
