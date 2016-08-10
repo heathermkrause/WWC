9521ea445c89ab42eac354e5ce2df314a64573ed
juliasilge <julia.silge@gmail.com>
2016-08-10 13:03:26 -0600

    Update units tests for summarize_survey to use multiple geographies

--- tests/testthat/test-summarize-survey.R
+++ tests/testthat/test-summarize-survey.R
@@ -4,7 +4,7 @@ suppressPackageStartupMessages(library(dplyr))
 data(tinysurvey)
 
 test_that("can summarize a survey", {
-        expect_warning(sm <- weight_wwc(tinysurvey, OK, 
+        expect_warning(sm <- weight_wwc(tinysurvey, 
                                         sex, raceethnicity) %>% 
                                summarize_survey(response),
                        "Some strata absent from sample: ignored")
@@ -12,5 +12,5 @@ test_that("can summarize a survey", {
         expect_equal(length(sm), 3)
         expect_true(all(sm$result == c("Raw", "Weighted")))
         expect_is(sm$value, "numeric")
-        expect_true(sm$value[1] > sm$value[2])
+        expect_true(sm$value[1] < sm$value[2])
 })
