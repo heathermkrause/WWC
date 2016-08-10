8f5484d0774b481bedd67b4d2af714dc03db69bc
juliasilge <julia.silge@gmail.com>
2016-08-10 11:42:32 -0600

    Update unit tests for using geography in weighting

--- tests/testthat/test-weight-one.R
+++ tests/testthat/test-weight-one.R
@@ -3,12 +3,12 @@ context("Weighting using age ACS tables")
 data(tinysurvey)
 
 test_that("error handling for weighting indicator is working", {
-        expect_error(weight_age(tinysurvey, UT, howabouthemapples),
+        expect_error(weight_age(tinysurvey, howabouthemapples),
                      "indicator must be one of sex, raceethnicity, or age")
 })
 
 test_that("can weight a survey using age ACS tables", {
-        expect_warning(resultDF <- weight_age(tinysurvey, UT, 
+        expect_warning(resultDF <- weight_age(tinysurvey, 
                                               sex, raceethnicity),
                        "Some strata absent from sample: ignored")
         expect_is(resultDF, "tbl_df")
@@ -23,12 +23,12 @@ context("Weighting using education ACS tables")
 data(tinysurvey)
 
 test_that("error handling for weighting indicator is working", {
-        expect_error(weight_education(tinysurvey, UT, howabouthemapples),
+        expect_error(weight_education(tinysurvey, howabouthemapples),
                      "indicator must be one of sex, raceethnicity, or education")
 })
 
 test_that("can weight a survey using education ACS tables", {
-        expect_warning(resultDF <- weight_education(tinysurvey, UT, 
+        expect_warning(resultDF <- weight_education(tinysurvey, 
                                                     sex, raceethnicity),
                        "Some strata absent from sample: ignored")
         expect_is(resultDF, "tbl_df")
--- tests/testthat/test-weight-wwc.R
+++ tests/testthat/test-weight-wwc.R
@@ -1,19 +1,18 @@
 context("Weighting by one or more indicators")
 
-data(tinysurvey)
 
 test_that("error handling for weighting indicator names is working", {
-        expect_error(weight_wwc(tinysurvey, TX, howabouthemapples),
+        expect_error(weight_wwc(tinysurvey, howabouthemapples),
                      "indicators must be one or more of sex, raceethnicity, age, and education")
 })
 
 test_that("error handling for education + age is working", {
-        expect_error(weight_wwc(tinysurvey, TX, age, education),
+        expect_error(weight_wwc(tinysurvey, age, education),
                      "indicators cannot include both age and education")
 })
 
 test_that("can weight a survey", {
-        expect_warning(resultDF <- weight_wwc(tinysurvey, TX, 
+        expect_warning(resultDF <- weight_wwc(tinysurvey, 
                                               sex, raceethnicity),
                        "Some strata absent from sample: ignored")
         expect_is(resultDF, "tbl_df")
@@ -23,9 +22,22 @@ test_that("can weight a survey", {
 })
 
 test_that("can weight a survey with missing data", {
-        resultDF <- weight_wwc(surveymissing, TX, sex, raceethnicity)
+        resultDF <- weight_wwc(surveymissing, sex, raceethnicity)
         expect_is(resultDF, "tbl_df")
         expect_equal(length(surveymissing) + 1, length(resultDF))
-        expect_equal(dim(resultDF)[1], 620)
+        expect_equal(dim(resultDF)[1], 521)
+        expect_is(resultDF$weight, "numeric")
+})
+
+test_that("can weight a survey with more than one geography", {
+        resultDF <- weight_wwc(twostatessurvey, raceethnicity)
+        expect_is(resultDF, "tbl_df")
+        expect_equal(length(surveymissing) + 1, length(resultDF))
+        expect_true(all(twostatessurvey %>% 
+                                group_by(geography) %>% 
+                                summarise(n = n()) ==
+                                resultDF %>% 
+                                group_by(geography) %>% 
+                                summarise(n = n())))
         expect_is(resultDF$weight, "numeric")
 })
