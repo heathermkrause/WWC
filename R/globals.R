c6283e6ae9dd53c1d2a143816ca89fab9db64707
juliasilge <julia.silge@gmail.com>
2016-08-10 13:22:36 -0600

    OMG so many globals now. ☉_☉

--- R/globals.R
+++ R/globals.R
@@ -1,7 +1,8 @@
-globalVariables(c("population", "raceethnicity", "sex", "notother", "education",
-                  "age", "sextotal", "geototal", "value", "yes", "no", "prob",
-                  "age_od", "race_od", "sex_od", "edu_od", "odds", "region",
-                  "age_lambda", "race_lambda", "sex_lambda", "education_lambda",
-                  "response", "acsedutable", "acsagetable",
-                  "geo_lambda", "geo_od", "geoprob",
-                  "state", "county", "FIPS"))
+globalVariables(c("FIPS", "acsagetable", "acsedutable", "age", "age_lambda", 
+                  "age_od", "county", "data.x", "data.y", "edu_od", "education", 
+                  "education_lambda", "geo_lambda", "geo_od", "geography",
+                  "geoprob", "geototal", "geoweight", "no", "notother", "odds", 
+                  "population", "prob", "race_lambda", "race_od", 
+                  "raceethnicity", "region", "response", "sex", "sex_lambda", 
+                  "sex_od", "sextotal", "state", "surveytotal", "value", 
+                  "weight", "yes"))
