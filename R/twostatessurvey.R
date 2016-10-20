dc84084512c44d6dc5d2f13e7e15fbf80f58d934
juliasilge <julia.silge@gmail.com>
2016-10-20 17:01:00 -0600

    Update documentation for example surveys for new BIGGER AGE BINS

--- R/twostatessurvey.R
+++ R/twostatessurvey.R
@@ -13,7 +13,7 @@
 #' \item{raceethnicity}{White alone (not Hispanic or 
 #' Latino), Hispanic or Latino, black alone, Asian alone, or other}
 #' \item{age}{Under 18 years, 18 to 24 years, 25 to 44 years, 45 to 64 years,  
-#' or 65 years and over.}
+#' or 65 years and over}
 #' \item{education}{Less than high school diploma, high school graduate 
 #' (includes equivalency), some college or associate's degree, bachelor's 
 #' degree or higher}
