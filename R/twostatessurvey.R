519ce1ca6e57cd0739f561ba4bcfe49e8b2bb8da
juliasilge <julia.silge@gmail.com>
2016-10-20 16:51:09 -0600

    Update age bins in docs for example surveys

--- R/twostatessurvey.R
+++ R/twostatessurvey.R
@@ -12,10 +12,8 @@
 #' \item{sex}{Either male or female}
 #' \item{raceethnicity}{White alone (not Hispanic or 
 #' Latino), Hispanic or Latino, black alone, Asian alone, or other}
-#' \item{age}{Under 5 years, 5 to 9 years, 10 to 14 years, 15 to 17 years, 18 
-#' and 19 years, 20 to 24 years, 25 to 29 years, 30 to 34 years, 35 to 44 years, 
-#' 45 to 54 years, 55 to 64 years, 65 to 74 years, 75 to 84 years, 85 years and 
-#' over}
+#' \item{age}{Under 18 years, 18 to 24 years, 25 to 44 years, 45 to 64 years,  
+#' or 65 years and over.}
 #' \item{education}{Less than high school diploma, high school graduate 
 #' (includes equivalency), some college or associate's degree, bachelor's 
 #' degree or higher}
