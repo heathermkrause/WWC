1bed0be6227cbb9fe782dc72915241df56dc7c61
juliasilge <julia.silge@gmail.com>
2016-10-25 10:50:16 -0600

    Move error handling for weighting indicators to its own function in utils

--- R/utils.R
+++ R/utils.R
@@ -1,4 +1,4 @@
-# utility function from tidyr::col_name
+## utility function from tidyr::col_name
 col_name <- function (x, default = stop("Please supply row/column name", call. = FALSE))
 {
         if (is.character(x)) return(x)
@@ -6,4 +6,16 @@ col_name <- function (x, default = stop("Please supply row/column name", call. =
         if (is.name(x)) return(as.character(x))
         if (is.null(x)) return(x)
         stop("Invalid row/column specification", call. = FALSE)
+}
+
+## error handling for for weighting indicators
+test_indicators <- function(dots, force_edu) {
+        if (any(purrr::map(dots, function(x) 
+        {x[[1]] %in% c("sex", "raceethnicity", "age", "education")}) == FALSE)) {
+                stop("indicators must be one or more of sex, raceethnicity, age, and education") }
+        if (sum(purrr::map_lgl(dots, function(x) {x[[1]] %in% c("age", "education")})) > 1) {
+                stop("indicators cannot include both age and education") }
+        if ("age" %in% dots & force_edu) {
+                stop("force_edu cannot be TRUE if age is one of the indicators") }
+        return(TRUE)
 }
--- R/weight_wwc.R
+++ R/weight_wwc.R
@@ -48,13 +48,7 @@ weight_wwc <- function(mysurvey, ..., force_edu = FALSE) {
 weight_wwc_ <- function(mysurvey, dots, force_edu = FALSE) {
         
         # error handling for weighting indicator
-        if (any(purrr::map(dots, function(x) 
-        {x[[1]] %in% c("sex", "raceethnicity", "age", "education")}) == FALSE)) {
-                stop("indicators must be one or more of sex, raceethnicity, age, and education") }
-        if (sum(purrr::map_lgl(dots, function(x) {x[[1]] %in% c("age", "education")})) > 1) {
-                stop("indicators cannot include both age and education") }
-        if ("age" %in% dots & force_edu) {
-                stop("force_edu cannot be TRUE if age is one of the indicators") }
+        test_indicators(dots, force_edu)
         
         # exclude rows/observations/respondents who have NA for geography
         mysurvey <- mysurvey[!is.na(mysurvey$geography),]
