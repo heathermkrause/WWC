87c0b5d58b01bf624bd6fee394dade5915b72bb5
juliasilge <julia.silge@gmail.com>
2016-11-08 10:00:12 -0700

    Add a CSV of weighted survey results

--- data-raw/write_csv.R
+++ data-raw/write_csv.R
@@ -1,4 +1,8 @@
 ### script to write example csv to be read in weight_csv ----------------------
 
 library(readr)
+library(WWC)
 write_csv(texassurvey, "inst/extdata/examplesurvey.csv")
+
+write_csv(weight_wwc(twostatessurvey, sex, raceethnicity),
+          "inst/extdata/weightedsurvey.csv")
